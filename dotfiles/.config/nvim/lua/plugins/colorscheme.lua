59a4892fa2f9f7e1960a98cd73304bae2bf0d974
Graeme Hill <graemekh@gmail.com>
2026-02-07 00:07:29 -0800

    theme?

---
+++ dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
