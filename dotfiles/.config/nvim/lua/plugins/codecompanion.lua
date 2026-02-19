4c276fdc5e61e9071ad73930660ca7c30fa27d16
Graeme Hill <graemekh@gmail.com>
2026-02-18 19:37:50 -0800

    change codecompanion hotkeys

--- dotfiles/.config/nvim/lua/plugins/codecompanion.lua
+++ dotfiles/.config/nvim/lua/plugins/codecompanion.lua
@@ -31,10 +31,12 @@ return {
       },
     },
     keys = {
-      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Code Companion Actions" },
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = "n", desc = "Code Companion Actions" },
+      { "<leader>aa", ":'<,'>CodeCompanionActions<cr>", mode = "v", desc = "Code Companion Actions" },
       { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle Chat" },
-      { "<leader>ai", "<cmd>CodeCompanion<cr>", mode = { "n", "v" }, desc = "Inline Assistant" },
-      { "<leader>ap", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add Selection to Chat" },
+      { "<leader>ai", "<cmd>CodeCompanion<cr>", mode = "n", desc = "Inline Assistant" },
+      { "<leader>ai", ":'<,'>CodeCompanion<cr>", mode = "v", desc = "Inline Assistant" },
+      { "<leader>ap", ":'<,'>CodeCompanionChat Add<cr>", mode = "v", desc = "Add Selection to Chat" },
     },
   },
 }
