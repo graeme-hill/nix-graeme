0f32273d4a8a279f2a98da6d2c025d1c9ce2143e
Graeme Hill <graemekh@gmail.com>
2026-02-18 23:06:38 -0800

    commands for codecompanion

--- dotfiles/.config/nvim/lua/plugins/codecompanion.lua
+++ dotfiles/.config/nvim/lua/plugins/codecompanion.lua
@@ -30,6 +30,25 @@ return {
         end,
       },
     },
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
+
+      -- Custom "implement this" command
+      vim.keymap.set("v", "<leader>ax", function()
+        vim.cmd("'<,'>CodeCompanion #buffer implement this")
+      end, { desc = "Implement selection" })
+
+      vim.keymap.set("n", "<leader>ax", function()
+        local line = vim.fn.line(".")
+        local col = vim.fn.col(".")
+        local current_line = vim.fn.getline(".")
+        local prompt = string.format(
+          "#buffer implement the code at line %d (cursor at column %d). The line content is: %s",
+          line, col, current_line
+        )
+        vim.cmd("CodeCompanion " .. prompt)
+      end, { desc = "Implement at cursor" })
+    end,
     keys = {
       { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = "n", desc = "Code Companion Actions" },
       { "<leader>aa", ":'<,'>CodeCompanionActions<cr>", mode = "v", desc = "Code Companion Actions" },
@@ -37,6 +56,7 @@ return {
       { "<leader>ai", ":CodeCompanion #buffer ", mode = "n", desc = "Inline Assistant (with buffer)" },
       { "<leader>ai", ":'<,'>CodeCompanion ", mode = "v", desc = "Inline Assistant" },
       { "<leader>ap", ":'<,'>CodeCompanionChat Add<cr>", mode = "v", desc = "Add Selection to Chat" },
+      { "<leader>ax", desc = "Implement this" },
     },
   },
 }
