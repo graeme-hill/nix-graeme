eeb434effc854097afd7064a242ef6ebcf57d071
Graeme Hill <graemekh@gmail.com>
2026-02-20 00:19:49 -0800

    attempt at mic fix on zenbook

--- hosts/zenbook/default.nix
+++ hosts/zenbook/default.nix
@@ -5,6 +5,9 @@
     /etc/nixos/hardware-configuration.nix
   ];
 
+  # Lunar Lake requires a newer kernel for SOF audio topology ABI compatibility
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   myHost = {
     # Zenbook monitor config
     monitorConfig = "monitor=,preferred,auto,1.5";
