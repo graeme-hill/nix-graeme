8d53caf11dcff1abbc23e7f4759391cbd60cd35c
Graeme Hill <graemekh@gmail.com>
2026-02-21 14:43:42 -0800

    amd driver dlags

--- hosts/donkey/default.nix
+++ hosts/donkey/default.nix
@@ -7,9 +7,12 @@
     ../../modules/system/gaming.nix
   ];
 
+
   # AMD P-State for better CPU scaling
   boot.kernelParams = [
     "amd_pstate=active"
+    "amdgpu.runpm=0"    # Disable dGPU runtime PM (prevents dual-GPU power cycling crashes)
+    "amdgpu.gfx_off=0"  # Disable GFX power gating
   ];
 
   # AMD GPU specific settings
