23abe1fb156435fc9bcccdfbffcf971c196d9f61
Graeme Hill <graemekh@gmail.com>
2026-02-21 22:08:26 -0800

    zig for some reason

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
     -- import/override with your plugins
     { import = "plugins" },
   },
